9635db1fb91f0be25652c3bde48fedf1108fee14
killjoy <researchuse@gmail.com>
2022-07-29 20:43:28 +0530

    testing pir

--- AC_onoff_for_soet.ino
+++ AC_onoff_for_soet.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#define pir1 D7
+#define pir2 D8
 
+int p1_val, p2_val;
+
+void setup() {
+
+  Serial.begin(115200);
+  pinMode(pir1, INPUT);
+  pinMode(pir2, INPUT);
+
+  // put your setup code here, to run once:
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  p1_val = digitalRead(pir1);
+  p2_val = digitalRead(pir2);
 
+  Serial.print(p1_val);
+  Serial.println(p2_val);
+
+  // put your main code here, to run repeatedly:
 }
